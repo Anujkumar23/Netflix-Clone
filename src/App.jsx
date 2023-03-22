603ddc2673de35ec63960e8093c9cade191aede1
anuj <anujthakur315@gmail.com>
2023-03-22 20:59:53 +0530

    add Routes to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,67 @@
 import React from 'react'
-import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { AuthProvider, useAuth } from '../common/auth'
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Navigate, Outlet, Route, RouterProvider } from 'react-router-dom'
 import Layout from './component/Layout'
+import Profile from './pages/Profile'
 import Browse from './Pages/Browse'
+import Login from './component/Login'
+import Register from './component/Register'
+
+
+
+
+// function ProtectedRoute({children}){
+//   const {user}=useAuth();
+ 
+//     if(!user){
+//     return <Navigate to="/login"/>
+//     }
+//     else{
+//       return children;
+//     }
+
+ 
+// }
+
 
 
 const routes=createBrowserRouter(createRoutesFromElements(
   <>
-  <Route path='/' element={<h1>Hello world</h1>}/>
-   <Route path='/login' element={<h1>Login</h1>}/>
-  
+  <Route  path='/' element={<Outlet/>}>
+  <Route index element={<Profile/>}/>
+  <Route path='manageProfile' element={<Profile edit/>}/>
 
-  <Route path='/browse' element={<Layout/>}>
-    <Route index element={<Browse/>} />
-  </Route>
-  <Route path='/latest' element={<Layout/>}>
+  <Route path='latest' element={<Layout/>}>
     <Route index element={<h1>Hello latest</h1>} />
   </Route>
+  <Route path='browse' element={<Layout/>}>
+    <Route index element={<Browse/>} />
+  </Route>
+  </Route>
+
+
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/register' element={<Register/>}/>
   </>
 ))
 
+  
+
+  
+  
+
+  
+
 function App() {
   return (
-    <RouterProvider router={routes}/>
+  
+     <RouterProvider router={routes}/>
+
+    
+
+     
+
+    
   )
 }
 
